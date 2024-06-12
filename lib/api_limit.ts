27d914eb2b7f246f7f0d6668fa3639b510084b47
JimmyM <DavidYangPersonal@outlook.com>
2024-06-13 00:10:33 +0800

    Comment: increase api limit

--- lib/api_limit.ts
+++ lib/api_limit.ts
@@ -3,32 +3,12 @@ import { auth } from '@clerk/nextjs';
 import prismadb from './prismadb';
 import { MAX_FREE_COUNTS } from '@/constant';
 
-export const increaseApiLimit = async () => {
-  const { userId } = auth();
-  if (!userId) {
-    return;
-  }
-
-  const userApiLimit = await prismadb.userApiLimit.findUnique({
-    where: {
-      userId,
-    },
-  });
-
-  if (userApiLimit) {
-    await prismadb.userApiLimit.update({
-      where: { userId: userId },
-      data: { count: userApiLimit.count + 1 },
-    });
-  } else {
-    await prismadb.userApiLimit.create({
-      data: {
-        userId,
-        count: 1,
-      },
-    });
-  }
-};
+/**
+ * 增加用户的 API 调用次数限制，每次调用增加 1。
+ * 如果用户已经存在，则增加他的调用次数；如果用户不存在，则创建一个新的记录，调用次数初始化为 1。
+ *
+ * @return {Promise<void>} 一个 Promise，当函数执行完成后被解析。
+ */
 
 // 这个函数用来ApiLimit是否已经达到了上限（MAX_FREE_COUNTS）
 export const checkApiLimit = async () => {
